9968c3a37a54ca6b5421b6180e758f3814e46cc7
paddured <104554472+paddured@users.noreply.github.com>
2026-09-25 00:08:18 +0530

    Update greeting message in App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -3,7 +3,7 @@ package com.example;
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Hello from DevOps Lab!");
+      System.out.println("Hello from DevOps Lab - Continuous Integration!");
         System.out.println("BITS ID: 2022wb86197");
     }
 }
