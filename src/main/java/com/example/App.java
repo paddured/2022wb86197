ca6fa422533a2a2c3fad3a5d694ba60fde4a9648
Padmasri <2022wb86197@wilp.bits-pilani.ac.in>
2026-09-24 12:13:31 +0530

    Initial Maven project

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,10 @@
+package com.example;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello from DevOps Lab!");
+        System.out.println("BITS ID: 2022wb86197");
+    }
+}
+
